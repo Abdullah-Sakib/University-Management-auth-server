806386eacaf4a570b5026807aa757af4b960335f
Abdullah-Sakib <abdullahsakib888@gmail.com>
2023-05-29 17:15:27 +0600

    [DONE] : setup express, mongoose, cors, parser

---
+++ src/server.ts
@@ -0,0 +1,17 @@
+import mongoose from "mongoose";
+import app from "./app";
+import confit from "./config/index";
+
+async function bootstrap() {
+  try {
+    await mongoose.connect(confit.database_url as string);
+    console.log("✅ Database connected successfully");
+    app.listen(confit.port, () => {
+      console.log(`Application listening on port ${confit.port}`);
+    });
+  } catch (err) {
+    console.log("❌ Failed to connect database.", err);
+  }
+}
+
+bootstrap();
