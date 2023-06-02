487fce28f9e654d9846286b449cce8d31d11d072
Abdullah-Sakib <abdullahsakib888@gmail.com>
2023-06-02 20:17:27 +0600

    [DONE] customized or formatted the log so that it become more readable and nice looking

--- src/shared/logger.ts
+++ src/shared/logger.ts
@@ -1,15 +1,25 @@
-import winston from 'winston'
+import { createLogger, format, transports } from 'winston'
 import path from 'path'
 import process from 'process'
+const { combine, timestamp, label, printf, prettyPrint } = format
+
+// Custom format
+const myFormat = printf(({ level, message, label, timestamp }) => {
+  const date = new Date(timestamp)
+  const hour = date.getHours()
+  const minute = date.getMinutes()
+  const second = date.getSeconds()
+  return `${`[${date.toDateString()} ${hour}:${minute}:${second}]`} [${label}] ${level}: ${message}`
+})
 
 // Create a logger for general information logs
-const logger = winston.createLogger({
+const logger = createLogger({
   level: 'info',
-  format: winston.format.json(),
+  format: combine(label({ label: 'UM' }), timestamp(), myFormat, prettyPrint()),
   defaultMeta: { service: 'user-service' },
   transports: [
-    new winston.transports.Console(), // Log to the console
-    new winston.transports.File({
+    new transports.Console(), // Log to the console
+    new transports.File({
       // Log to a file
       filename: path.join(process.cwd(), 'logs', 'winston', 'success.log'), // Specify the file path for success logs
       level: 'info',
@@ -18,13 +28,13 @@ const logger = winston.createLogger({
 })
 
 // Create a logger specifically for error logs
-const errorlogger = winston.createLogger({
+const errorlogger = createLogger({
   level: 'error',
-  format: winston.format.json(),
+  format: combine(label({ label: 'UM' }), timestamp(), myFormat, prettyPrint()),
   defaultMeta: { service: 'user-service' },
   transports: [
-    new winston.transports.Console(), // Log errors to the console
-    new winston.transports.File({
+    new transports.Console(), // Log errors to the console
+    new transports.File({
       // Log errors to a file
       filename: path.join(process.cwd(), 'logs', 'winston', 'error.log'), // Specify the file path for error logs
       level: 'error',
