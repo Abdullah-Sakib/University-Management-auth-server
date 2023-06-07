e8bfdbff42882aca720ee79c8fde8433e62b470f
Abdullah-Sakib <abdullahsakib888@gmail.com>
2023-06-07 22:12:47 +0600

    [DONE] Added comment in catchAsync and sendResponse page

--- src/shared/catchAsync.ts
+++ src/shared/catchAsync.ts
@@ -1,11 +1,12 @@
 import { NextFunction, Request, RequestHandler, Response } from 'express';
 
+// Function to wrap an asynchronous request handler with error handling
 const catchAsync = (func: RequestHandler) => {
   return async (req: Request, res: Response, next: NextFunction) => {
     try {
-      func(req, res, next);
+      func(req, res, next); // Call the provided request handler function
     } catch (error) {
-      next();
+      next(); // Call the next middleware or error handler in case of an error
     }
   };
 };
--- src/shared/sendResponse.ts
+++ src/shared/sendResponse.ts
@@ -1,5 +1,6 @@
 import { Response } from 'express';
 
+// Define a generic interface for the API response
 type IApiResponse<T> = {
   statusCode: number;
   success: boolean;
@@ -7,14 +8,15 @@ type IApiResponse<T> = {
   data?: T | null;
 };
 
+// Function to send the API response
 const sendResponse = <T>(res: Response, data: IApiResponse<T>) => {
   const responseData: IApiResponse<T> = {
-    statusCode: data?.statusCode,
-    success: data?.success,
-    message: data?.message,
-    data: data?.data || null,
+    statusCode: data?.statusCode, // Assign the status code from the data object
+    success: data?.success, // Assign the success flag from the data object
+    message: data?.message, // Assign the message from the data object
+    data: data?.data || null, // Assign the data from the data object or null if it's undefined
   };
-  res.status(data?.statusCode).json(responseData);
+  res.status(data?.statusCode).json(responseData); // Set the response status code and send the JSON response
 };
 
 export default sendResponse;
